a50cccf78bc4a9ab1ce8a64cf228e1979edd6f73
Rafael Camargo <rafael.camargo.sp@gmail.com>
2015-09-24 15:58:24 -0300

    Integrando front com back

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,7 +1,7 @@
 var contatos = [
-	{ _id: 1, nome: 'Contato Exemplo 1', email: 'cont1@empresa.com.br' },
-	{ _id: 2, nome: 'Contato Exemplo 2', email: 'cont2@empresa.com.br' },
-	{ _id: 3, nome: 'Contato Exemplo 3', email: 'cont3@empresa.com.br' }
+	{ _id: 1, nome: 'Contato Exemplo 1 Express', email: 'cont1@empresa.com.br' },
+	{ _id: 2, nome: 'Contato Exemplo 2 Express', email: 'cont2@empresa.com.br' },
+	{ _id: 3, nome: 'Contato Exemplo 3 Express', email: 'cont3@empresa.com.br' }
 ];
 
 module.exports = function () {
--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,25 +1,20 @@
-angular.module('contatooh').controller('ContatosController',
-	function ($scope) {
-		// código omitido
-		$scope.contatos = [
-			{
-				"_id": 1,
-				"nome": "Contato Angular 1",
-				"email": "cont1@empresa.com.br"
-			},
-			{
-				"_id": 2,
-				"nome": "Contato Angular 2",
-				"email": "cont2@empresa.com.br"
-			},
-			{
-				"_id": 3,
-				"nome": "Contato Angular 3",
-				"email": "cont3@empresa.com.br"
-			}
-		];
+angular.module('contatooh').controller('ContatosController', function ($scope, $http) {
 		
-		$scope.filtro = '';
-		
-		
+	$scope.contatos = [];
+	$scope.total = 0;
+	$scope.filtro = '';
+	
+	$scope.incrementa = function () {
+		$scope.total++;
+	};
+	
+	$http.get('/contatos')
+	.success(function (data) {
+		$scope.contatos = data;
+	})
+	.error(function (statusText) {
+		console.log("Não foi possível obter a lista de contatos");
+		console.log(statusText);
 	});
+			
+});
