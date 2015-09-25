ab21961dc8c12477f4f1f745eca6811aaae9475a
Rafael Camargo <rafael.camargo.sp@gmail.com>
2015-09-25 14:58:38 -0300

    Fazendo CRUD do Contato no MongoDB

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,63 +1,76 @@
-var contatos = [
-	{ _id: 1, nome: 'Contato Exemplo 1 Express', email: 'cont1@empresa.com.br' },
-	{ _id: 2, nome: 'Contato Exemplo 2 Express', email: 'cont2@empresa.com.br' },
-	{ _id: 3, nome: 'Contato Exemplo 3 Express', email: 'cont3@empresa.com.br' }
-];
+module.exports = function (app) {
 
-module.exports = function () {
-
-	var ID_CONTATO_INC = 3;
+	var Contato = app.models.contato;
 	var controller = {};
 
-	controller.listaContatos = function (req, res) { 
-		// envia a lista
-		res.json(contatos);
-	};
-
-	controller.removeContato = function (req, res) {
-		var idContato = req.params.id;
-		contatos = contatos.filter(function (contato) {
-			return contato._id != idContato;
-		});
-		res.status(204).end();
+	controller.listaContatos = function (req, res) {
+		var promise = Contato.find().exec()
+			.then(
+				function (contatos) {
+					res.json(contatos);
+				},
+				function (erro) {
+					console.error(erro);
+					res.status(500).json(erro);
+				}
+				);
 	};
 
 	controller.obtemContato = function (req, res) {
+		var _id = req.params.id;
+		Contato.findById(_id).exec()
+			.then(
+				function (contato) {
+					if (!contato) throw new Error("Contato não encontrado");
+					res.json(contato);
+				},
+				function (erro) {
+					console.log(erro);
+					res.status(404).json(erro);
+				}
+				);
+	 };
 
-		var idContato = req.params.id;
-		var contato = contatos.filter(function (contato) {
-			return contato._id == idContato;
-		})[0];
-
-		contato ?
-			res.json(contato) :
-			res.status(404).send('Contato não encontrado');
-	};
-
+	controller.removeContato = function (req, res) {
+		var _id = req.params.id;
+		Contato.remove({ "_id": _id }).exec()
+			.then(
+				function () {
+					res.status(204).end();
+				},
+				function (erro) {
+					return console.error(erro);
+				}
+				);
+	 };
 
 	controller.salvaContato = function (req, res) {
-		var contato = req.body;
-		contato = contato._id ?
-			atualiza(contato) :
-			adiciona(contato);
-		res.json(contato);
-	};
+		var _id = req.body._id;
+		if (_id) {
+			Contato.findByIdAndUpdate(_id, req.body).exec()
+				.then(
+					function (contato) {
+						res.json(contato);
+					},
+					function (erro) {
+						console.error(erro);
+						res.status(500).json(erro);
+					}
+					);
+		} else {
+			Contato.create(req.body)
+				.then(
+					function (contato) {
+						res.status(201).json(contato);
+					},
+					function (erro) {
+						console.log(erro);
+						res.status(500).json(erro);
+					}
+					);
+		}
 
-	function adiciona(contatoNovo) {
-		contatoNovo._id = ++ID_CONTATO_INC;
-		contatos.push(contatoNovo);
-		return contatoNovo;
-	}
-
-	function atualiza(contatoAlterar) {
-		contatos = contatos.map(function (contato) {
-			if (contato._id == contatoAlterar._id) {
-				contato = contatoAlterar;
-			}
-			return contato;
-		});
-		return contatoAlterar;
-	}
+	 };
 
 	return controller;
-}; 
+};
