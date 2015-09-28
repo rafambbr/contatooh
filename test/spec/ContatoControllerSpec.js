15dac3e943911218275f77be4c744f73fea0b4ff
Rafael Camargo <rafael.camargo.sp@gmail.com>
2015-09-28 11:54:04 -0300

    $httpBackend: “mockando” nosso backend

--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -1,13 +1,20 @@
 describe("ContatoController", function () {
 
 	var $scope;
+	var $httpBackend;
 
 	beforeEach(function () {
 		//carregando o módulo 'contatooh' do AngularJS para os testes funcionarem
 		module('contatooh');
-		
-		inject(function ($injector) {
+
+		inject(function ($injector, _$httpBackend_) {
+
 			$scope = $injector.get('$rootScope').$new();
+			$httpBackend = _$httpBackend_;
+
+			$httpBackend.when('GET', '/contatos/1').respond({ _id: '1' });
+			$httpBackend.when('GET', '/contatos').respond([{}]);
+
 		});
 	});
 
@@ -15,11 +22,26 @@ describe("ContatoController", function () {
 	it("Deve criar um Contato vazio quando nenhum parâmetro de rota for passado", inject(function ($controller) {
 		
 		//Injeta o Controller e todas suas dependências são resolvidas
-		$controller('ContatoController', {"$scope" : $scope});
-
+		$controller('ContatoController', {
+			'$routeParams': { contatoId: 1 },
+			"$scope": $scope
+		});
+		$httpBackend.flush();
+ 
 		//Todas as funções suportadas por expect => http://jasmine.github.io/2.0/introduction.html
-		expect($scope.contato._id).toBeUndefined();
+		expect($scope.contato._id).toBeDefined();
 
+	}));
+
+	it("Deve preencher o Contato quando parâmetro de rota for passado", inject(function ($controller) {
+		
+		$controller('ContatoController', {
+			$routeParams: { contatoId: 1 },
+			'$scope': $scope
+		});
+		$httpBackend.flush();
+		
+		expect($scope.contato._id).toBeDefined();
 	}));
 
 });
