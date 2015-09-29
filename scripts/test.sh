9f41c60ddd9a85ceef03084433c9e5d93c4e043f
Rafael Camargo <rafael.camargo.sp@gmail.com>
2015-09-29 09:28:30 -0300

    fix conf.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,5 @@ export SELENIUM_USER=contatooh01
 export SELENIUM_USER_PASSWORD=contatooh01DEV
 export CLIENT_ID=ec5e077c962d7894a29f
 export CLIENT_SECRET=c539c578795bae522313bbc84e5d642dea64b1da
-# protractor config/protractor.js
-karma start config/karma.config.js --single-run --browsers PhantomJS
+protractor config/protractor.js
+# karma start config/karma.config.js --single-run --browsers PhantomJS
