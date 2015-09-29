07f0f64a68c8c1d8b169c49e82950dad82f05a67
Rafael Camargo <rafael.camargo.sp@gmail.com>
2015-09-29 12:39:49 -0300

    Add. diretiva 'meuBotao'

--- public/js/directives/meus-componentes/meus-componentes.js
+++ public/js/directives/meus-componentes/meus-componentes.js
@@ -1,15 +1,34 @@
-angular.module('meusComponentes', []).directive('meuPainel', function () {
+angular.module('meusComponentes', [])
+
+	.directive('meuPainel', function () {
 		var directive = {}
 		directive.restrict = 'EA';
 		directive.transclude = true;
-		
+
 		directive.scope = {
 			titulo: '@'
 		};
-		
+
 		directive.transclude = true;
-		
+
 		directive.templateUrl = 'js/directives/meus-componentes/meu-painel.html';
-		
+
 		return directive;
-});
+	})
+
+	.directive('meuBotaoAviso', function () {
+		var directive = {}
+		directive.restrict = 'E';
+
+		directive.scope = {
+			nome: '@',
+			acao: '&'
+		};
+
+		directive.template =
+			'<button ng-click="acao()" class="btn btn-warning">'
+		+ '{{nome}}'
+		+ '</button>';
+
+		return directive;
+	});
