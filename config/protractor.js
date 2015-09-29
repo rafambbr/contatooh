92f12210581b2262a9160d624615d16d3a7f26c1
Rafael Camargo <rafael.camargo.sp@gmail.com>
2015-09-29 08:47:12 -0300

    fix

--- config/protractor.js
+++ config/protractor.js
@@ -1,21 +1,25 @@
 var config = require('./config')();
-exports.config = { 
-  sauceUser : config.sauceUser, 
-  sauceKey : config.sauceKey, 
-  capabilities : {
-    'name': config.sauceTestName,
-    'browserName': 'chrome',
-    'tunnel-identifier': config.travisJobNumber,
-    'build': config.travisBuild
-  },
 
-  specs: ['../test/e2e/**/*Spec.js'],
-  onPrepare: function() {
-    browser.driver.get('http://localhost:3000').then(function() {
-      browser.driver.findElement(by.id('entrar')).click()
-      browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
-      browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
-      browser.driver.findElement(by.name('commit')).click();
-    });
-  }
+exports.config = {
+	sauceUser: config.sauceUser,
+	sauceKey: config.sauceKey,
+	capabilities: {
+		'name': config.sauceTestName,
+		'browserName': 'chrome',
+		'tunnel-identifier': config.travisJobNumber,
+		'build': config.travisBuild
+	},
+	specs: ['../test/e2e/**/*Spec.js'],
+	onPrepare: function () {
+		
+		//'onPrepare' => é executado apenas uma vez antes dos testes
+		//Aqui usamos  o 'browser.driver' pois a pagina do GitHub não é feita em AngularJS
+		browser.driver.get('http://localhost:3000');
+		browser.driver.findElement(by.id('entrar')).click();
+		browser.driver.findElement(by.id('login_field'))
+			.sendKeys(config.seleniumUser);
+		browser.driver.findElement(by.id('password'))
+			.sendKeys(config.seleniumUserPassword);
+		browser.driver.findElement(by.name('commit')).click();
+	}
 };
